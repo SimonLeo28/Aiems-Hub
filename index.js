e97f40b678d81535e9f374423262e83ad8268cd0
SimonLeo28 <simonleoalexanderpcmct2023@gmail.com>
2025-01-17 15:01:26 +0530

    second commit

--- index.js
+++ index.js
@@ -5,12 +5,21 @@ import mongoose from "mongoose"
 const app = express()
 const port = 3000;
 
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended : true}))
 
-app.get("/", (req,res) => {
-    res.render("index.ejs")
-})
+app.get("/", (req, res) => {
+    res.render("index", { active: "home" });
+  });
+  
+  app.get("/materials", (req, res) => {
+    res.render("index", { active: "materials" });
+  });
+  
+  app.get("/contact", (req, res) => {
+    res.render("index", { active: "contact" });
+  });
 
 
 app.listen(port, () => {
